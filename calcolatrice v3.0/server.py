ffac704f5062549add636f88186e71bb2b1d4c45
thomashalo11 <thomastresca11@gmail.com>
2024-02-02 08:30:31 +0100

    Update server.py

--- calcolatrice v3.0/server.py
+++ calcolatrice v3.0/server.py
@@ -3,6 +3,7 @@ from threading import Thread
 
 SERVER_ADDRESS = "127.0.0.1"
 SERVER_PORT = 22224
+BUFFER_SIZE = 1024
 
 """_summary_
 La funzione avvia_server crea un endpoint di ascolto (sock_listen) dal
@@ -15,7 +16,46 @@ di uno specifico client.
 """
 
 def ricevi_comandi(sock_service, addr_client):
-    sock_service.close()
+    with sock_service as sock_client:
+        while True:
+            dati = sock_client.recv(BUFFER_SIZE).decode()
+            if not dati:
+                break
+            msg = json.loads(dati)
+            
+            if msg == "Esci":
+                    print("Connessione conclusa")
+                    break
+                
+            risultato = 0
+            
+            print("Lista: ", msg)
+            
+            operando1 = msg["primoNumero"]
+            operazione = msg["operazione"]
+            operando2 = msg["secondoNumero"]
+            
+            print(operando1)
+            print(operazione)
+            print(operando2)
+            
+            # Trasformiamo le stringhe in int
+            num1 = int(operando1)
+            num2 = int(operando2)
+            
+            if operazione == "+":
+                risultato = num1 + num2
+            elif operazione == "-":
+                risultato = num1 - num2
+            elif operazione == "/":
+                risultato = num1 / num2
+            elif operazione == "*":
+                risultato = num1 * num2
+                
+            output = str(risultato)
+            output = json.dumps(output)
+            output = output.encode()
+            sock_client.send(output)
 
 def ricevi_connessioni(sock_listen):
     while True:
